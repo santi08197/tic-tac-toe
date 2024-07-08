fc39e79e12a600858efbf6fd83de81b6f1e19e66
Santiago Gonzalez <santi08197@gmail.com>
2024-07-08 18:21:51 -0300

    Initial project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
+import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const TURNS = {
-    X:'x',
-    O:'o'
-  }
+const TURNS = {
+  X:'x',
+  O:'o'
+}
 
-  const board = Array(9).fill(null)
+const Square = ({children, updateBoard, index}) => {
   return (
-    <main>
+    <div className="square">
+      {children}
+    </div>
+  )
+}
+
+function App() {
+
+
+
+  const [board, setBoard] = useState(['x','o','','o','x','o','x','o','x'])
+  return (
+    <main className='board'>
       <h1>Tic Tac Toe</h1>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
     </main>
   )
 }
