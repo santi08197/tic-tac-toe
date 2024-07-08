088e1ca41ae363bd190e97a7111bc7c2cb36ffaf
Santiago Gonzalez <santi08197@gmail.com>
2024-07-08 17:41:43 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  const TURNS = {
+    X:'x',
+    O:'o'
+  }
+
+  const board = Array(9).fill(null)
+  return (
+    <main>
+      <h1>Tic Tac Toe</h1>
+    </main>
+  )
+}
+
+export default App
